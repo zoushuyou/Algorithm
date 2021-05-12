283e046d94bcbbdbb7dc62b0d65230b0937bf082
zsy <zsy4884@gmail.com>
2021-05-12 17:18:14 +0800

    动态规划  两道题

---
+++ src/leetcode/zsy/dynamic/num303/NumArray.java
@@ -0,0 +1,23 @@
+package leetcode.zsy.dynamic.num303;
+
+/**
+ * @author zousy
+ * @version v1.0
+ * @Description
+ * @date 2021-05-12 17:04
+ */
+public class NumArray {
+
+    int[] sum;
+
+    public NumArray(int[] nums) {
+        sum = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sum[i+1] =  sum[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return sum[right+1] - sum[left];
+    }
+}
