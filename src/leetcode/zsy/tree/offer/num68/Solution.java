6bf52dfb85213a6b55b005a999e88c4415e7223d
zsy <zsy4884@gmail.com>
2021-05-18 17:23:27 +0800

    二叉搜索树的最近公共祖先

---
+++ src/leetcode/zsy/tree/offer/num68/Solution.java
@@ -0,0 +1,46 @@
+package leetcode.zsy.tree.offer.num68;
+
+import basic.zsy.tree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author zousy
+ * @version v1.0
+ * @Description
+ * @date 2021-05-16 22:42
+ */
+public class Solution {
+
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        List<TreeNode> path_p = getPath(root, p);
+        List<TreeNode> path_q = getPath(root, q);
+        TreeNode ancestor = null;
+        for (int i = 0; i < path_p.size() && i < path_q.size(); ++i) {
+            if (path_p.get(i) == path_q.get(i)) {
+                ancestor = path_p.get(i);
+            } else {
+                break;
+            }
+        }
+        return ancestor;
+    }
+
+    public List<TreeNode> getPath(TreeNode root, TreeNode target) {
+        List<TreeNode> path = new ArrayList<TreeNode>();
+        TreeNode node = root;
+        while (node != target) {
+            path.add(node);
+            if (target.val < node.val) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+        path.add(node);
+        return path;
+    }
+
+}
