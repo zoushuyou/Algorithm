1a2734cae44f81e7c9b66e6da5804a6b578d1620
zsy <zsy4884@gmail.com>
2021-06-07 14:58:37 +0800

    动态规划  打家劫舍 重点是要推出动态方程

---
+++ src/leetcode/zsy/dynamic/num198/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.zsy.dynamic.num198;
+
+/**
+ * @author zousy
+ * @version v1.0
+ * @Description
+ * @date 2021-06-07 14:09
+ */
+public class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < length; i++) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[length - 1];
+    }
+}
