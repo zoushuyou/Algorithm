b6e4820adbb62e363fd65ab2db4afaed1cbbe02b
zsy <zsy4884@gmail.com>
2021-07-16 13:56:44 +0800

    滑动窗口  最长重复子数组

---
+++ src/leetcode/zsy/slidingWindow/num718/Solution.java
@@ -0,0 +1,72 @@
+package leetcode.zsy.slidingWindow.num718;
+
+/**
+ * @author zousy
+ * @version v1.0
+ * @Description
+ * @date 2021-07-16 10:07
+ */
+public class Solution {
+    public int findLength(int[] nums1, int[] nums2) {
+        int res = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                int k = 0;
+                while (i+k <nums1.length && j+k <nums2.length){
+                    if (nums1[i+k] == nums2[j+k]){
+                        k++;
+                    }else {
+                        break;
+                    }
+                }
+                res = Math.max(k,res);
+            }
+        }
+        return res;
+    }
+
+
+    public int findLengthLeetcode(int[] A, int[] B) {
+        int n = A.length, m = B.length;
+        int ret = 0;
+        for (int i = 0; i < n; i++) {
+            int len = Math.min(m, n - i);
+            int maxlen = maxLength(A, B, i, 0, len);
+            ret = Math.max(ret, maxlen);
+        }
+        for (int i = 0; i < m; i++) {
+            int len = Math.min(n, m - i);
+            int maxlen = maxLength(A, B, 0, i, len);
+            ret = Math.max(ret, maxlen);
+        }
+        return ret;
+    }
+
+    public int maxLength(int[] A, int[] B, int addA, int addB, int len) {
+        int ret = 0, k = 0;
+        for (int i = 0; i < len; i++) {
+            if (A[addA + i] == B[addB + i]) {
+                k++;
+            } else {
+                k = 0;
+            }
+            ret = Math.max(ret, k);
+        }
+        return ret;
+    }
+
+
+    public int findLengthDfs(int[] A, int[] B) {
+        int n = A.length, m = B.length;
+        int[][] dp = new int[n + 1][m + 1];
+        int ans = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                dp[i][j] = A[i] == B[j] ? dp[i + 1][j + 1] + 1 : 0;
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+        return ans;
+    }
+
+}
