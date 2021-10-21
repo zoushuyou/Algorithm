146c7080460d6c9cde13c1b394bdf41770c485c3
zsy <zsy4884@gmail.com>
2021-10-21 09:55:57 +0800

    有想到这种方法，但是不知道代码如何写

---
+++ src/leetcode/zsy/array/num48/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.zsy.array.num48;
+
+/**
+ * @author zousy
+ * @version v1.0
+ * @Description
+ * @date 2021-10-21 9:34
+ */
+public class Solution {
+
+    public void rotate(int[][] matrix){
+        int n = matrix.length;
+        for (int i = 0; i < n/2; i++) {
+            for (int j = i; j < n-i-1; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-j-1][i];
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n-i-1] = tmp;
+            }
+        }
+    }
+}
